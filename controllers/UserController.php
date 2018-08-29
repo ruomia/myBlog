1810688120b2851421f9a6175ce9ee92ec02c279
humie <hyp@czxy.com>
2018-08-29 09:59:44 +0800

    搭建 MVC

---
+++ controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+namespace controllers;
+
+class UserController
+{
+    public function hello()
+    {
+        echo 'hello';
+    }
+    public function world()
+    {
+        echo 'world';
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,12 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+//定义常量
+define('ROOT', dirname(__FILE__) . '/../');
+
+//实现类的自动加载
+function autoload($class)
+{
+    $path = str_replace('\\', '/', $class);
+    require_once(ROOT . $path . '.php');
+}
+spl_autoload_register('autoload');
+
+//添加路由：解析 URL 上的路径：控制器/方法
+// 获取 URL上的路径
+if( isset($_SERVER['PATH_INFO']) )
+{
+    $pathInfo = $_SERVER['PATH_INFO'];
+    // 根据 / 转成数组
+    $pathInfo = explode('/', $pathInfo);
+
+    // 得到控制器名和方法：
+    $controller = ucfirst($pathInfo[1]) . 'Controller';
+    $action = $pathInfo[2];
+}
+else
+{
+    $controller = 'IndexController';
+    $action = 'index';
+}
+
+//为控制添加命名空间
+$fullController = 'controllers\\'.$controller;
+
+$_C = new $fullController;
+$_C->$action();
+
+//加载视图
+// 参数一、加载的视图的文件名
+// 参数二、向视图中传的数据
+function view($viewFileName, $data = [])
+{
+    //解压数组成变量
+    extract($data);
+
+    $path = str_replace('.', '/', $viewFileName) . '.html';
+
+    //加载视图
+    require_once(ROOT . 'views/' . $path);
+}
